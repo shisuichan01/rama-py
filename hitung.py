41ab270fa18c2331715b3e70385db57c948ef06e
shisuichan01 <79549704+shisuichan01@users.noreply.github.com>
2021-03-26 08:26:46 +0700

    Create hitung.py

---
+++ hitung.py
@@ -0,0 +1,6 @@
+number1 = input("Angka Pertama: ")
+number2 = input("\nANgka Kedua: ")
+
+sum = float(number1) + float(number2)
+
+print("Penjumlahan dari {0} dan {1} adalah {2}" .format(number1, number2, sum)) 
