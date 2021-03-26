990225ab6e789b11f86ebae580f57165bef5e68a
shisuichan01 <79549704+shisuichan01@users.noreply.github.com>
2021-03-26 08:42:34 +0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -25,8 +25,8 @@ if choice == '1':
 elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
 elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+   print(num1,"×",num2,"=", multiply(num1,num2))
 elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
+   print(num1,"÷",num2,"=", divide(num1,num2))
 else:
    print("Invalid input")
