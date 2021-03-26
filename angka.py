1b1290786101ab1c360097dc439bc53bec65e213
shisuichan01 <79549704+shisuichan01@users.noreply.github.com>
2021-03-26 08:50:51 +0700

    Create angka.py

---
+++ angka.py
@@ -0,0 +1,14 @@
+print ("PROGRAM PYTHON MENGHITUNG NILAI TERKECIL & TERBESAR SERTA NILAI RATA-RATA")
+n = int(input("\nMasukan Jumlah Bilangan = "))
+bil = []
+tot = 0
+for x in range(n):
+    m=x+1
+    a = int(input("Bilangan ke %d = "%m))
+    bil.append(a)
+    tot += bil[x]
+    rata2 = tot / n
+
+print("\nBilangan Terkecil : %d" %min(bil))
+print("Bilangan Terbesar : %d" %max(bil))
+print("Nilai Rata-rata   : %0.2f" %rata2)
